31c0f40ad8ab6d5343deb940f18ae9623a6a3cc2
sha990403 <sha990403@gmail.com>
2022-11-09 16:18:16 +0900

    221109 SWEA D1

---
+++ SWEA/src/D1/SWEA_D1_2072_홀수만더하기.java
@@ -0,0 +1,31 @@
+package D1;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_D1_2072_홀수만더하기 {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(in.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#"+test_case+" ");
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			
+			int sum = 0; // 초기화
+			
+			for (int i = 0; i < 10; i++) {
+				int n = Integer.parseInt(st.nextToken());
+				
+				if (n % 2 == 1) sum += n;
+			}
+			
+			sb.append(sum).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
