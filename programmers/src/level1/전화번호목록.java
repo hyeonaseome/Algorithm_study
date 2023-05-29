e167db03b77b007928f26f6c1122934d0436a067
hyeonaseome <hyeona.seo.dev@gmail.com>
2023-05-30 00:46:16 +0900

    230530 프로그래머스 LEVEL 1 해시 - 전화번호목록

---
+++ programmers/src/level1/전화번호목록.java
@@ -0,0 +1,17 @@
+package level1;
+
+import java.util.*;
+
+class 전화번호목록 {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        boolean answer = true;
+        
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        return answer;
+    }
+}
